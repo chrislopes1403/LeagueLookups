28ba71aa87a6e7572aecacb663dec3ab396f8ddd
Chris <chrislopes1403@comcast.net>
2021-07-07 15:10:03 -0400

    https for actions

--- src/apis/riotAPI.js
+++ src/apis/riotAPI.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const GeneralApi =  axios.create({
-    baseURL:'http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/'
+    baseURL:'https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/'
 });
 
 const DetailsApi = axios.create({
-    baseURL:'http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/'
+    baseURL:'https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/'
 });
 
 
